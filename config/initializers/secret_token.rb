cd9f150a8bb7b1fbfbf9da8e3a4e45f99fb5a881
Azer Demir <azer.demir@gmail.com>
2012-05-18 22:25:43 +0300

    First commit for oldtape

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oldtape::Application.config.secret_token = '27e7b4d239063a837b4cd61178b80da436737ca19792690fc2d28fab791cb16a1e8700acf4fdb4269a2aecdb8b6708a5813ef108a52089382c925cd051a0adc6'
